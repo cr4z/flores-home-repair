d9498daf362f18c13969e15bf4b4fabf17d20a2a
cr4z <64384340+cr4z@users.noreply.github.com>
2022-03-19 19:25:21 -0500

    Add Marble Falls for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,9 +18,8 @@ const Home: NextPage = () => {
         <div className="row">
           <h1>Home Renovation Projects</h1>
           <p>
-            Flores Home Repair is a Hill Country owned business that has a long history of working
-            with customers to build projects to the highest specifications. We service the Hill
-            Country and adjacent areas.
+            Flores Home Repair is a Marble Falls owned business that has a long history of working
+            with customers to build projects to the highest specifications. We service the entire Hill Country and adjacent areas, just give us a call!
           </p>
         </div>
         <Showcase />
